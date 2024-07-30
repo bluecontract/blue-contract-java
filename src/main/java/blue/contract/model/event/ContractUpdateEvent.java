6927b4df529501149bcc2c1d6638e2b868c228ef
Piotr Martyniak <piotr@bizgle.com>
2024-07-30 12:35:11 +0200

    fix: ContractSimulator for external contracts

--- src/main/java/blue/contract/model/event/ContractUpdateEvent.java
+++ src/main/java/blue/contract/model/event/ContractUpdateEvent.java
@@ -3,7 +3,7 @@ package blue.contract.model.event;
 import blue.language.model.BlueId;
 import blue.language.model.Node;
 
-@BlueId("DgQKBxaG8m5xfuZSQDm3yJqGZusU8k7tuizzQgg2XMa5")
+@BlueId({"DgQKBxaG8m5xfuZSQDm3yJqGZusU8k7tuizzQgg2XMa5"})
 public class ContractUpdateEvent {
     private Node changeset;
 
